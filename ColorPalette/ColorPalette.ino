a6dd74c1d14f506ce33ccf85b996641a99d4d780
Adrian Menzel <menzeladrian@googlemail.com>
2017-12-10 21:55:00 +0100

    custom colors!
    other features implemented:
      - MQTT Broker authentication
      - Striping option
      - Alerts

--- ColorPalette/ColorPalette.ino
+++ ColorPalette/ColorPalette.ino
@@ -3,9 +3,15 @@
 #include <WiFiClient.h>
 #include <MQTTClient.h>
 
-const char* host = "192.168.0.17";
-const char* ssid = "Buschfunk";
-const char* password = "*************";
+//Your Wifi
+const char* ssid = "*****";
+const char* password = "*****";
+//Your MQTT
+const char* host = "*****";
+const char* mqttUser = "*****";
+const char* mqttPass = "*****";
+const String mqttPrefix = "*****";
+const char* mqttId = "*****";
 
 WiFiClient net;
 MQTTClient mqtt;
@@ -23,6 +29,9 @@ CRGB leds[NUM_LEDS];
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 bool lightsOn;
+bool christmas;
+int color;
+bool striped;
 
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
@@ -43,10 +52,13 @@ void setup() {
     //WIFI & MQTT
     WiFi.mode(WIFI_AP_STA);
     WiFi.begin(ssid, password);
+    
     mqtt.begin(host, net);
     connect();
     mqtt.onMessage(messageReceived);
     lightsOn = true;
+    christmas = true;
+    color = 0;
     
     Serial.println("Setup completed...");
 }
@@ -61,7 +73,7 @@ void loop()
     mqtt.loop();
 
     //FASTLED
-    ChangePalettePeriodically();
+    HandlePallete();
     
     static uint8_t startIndex = 0;
     startIndex = startIndex + 1; /* motion speed */
@@ -89,27 +101,51 @@ void loop()
 }
 
 void connect() {
-  while(WiFi.waitForConnectResult() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
-    Serial.println("WiFi connection failed. Retry.");
-  }
+  
+  //while(WiFi.waitForConnectResult() != WL_CONNECTED) {
+    //WiFi.begin(ssid, password);
+    //Serial.println("WiFi connection failed. Retry.");
+  //}
 
   Serial.print("Wifi connection successful - IP-Address: ");
   Serial.println(WiFi.localIP());
 
-  while (!mqtt.connect(host)) {
+  while (!mqtt.connect(mqttId, mqttUser, mqttPass)) {
     Serial.print(".");
   }
 
-  mqtt.subscribe("/lights/all");
-  mqtt.subscribe("/lights/christmas");
+  mqtt.subscribe(mqttPrefix + "/lights/all");
+  mqtt.subscribe(mqttPrefix + "/lights/christmas");
+  mqtt.subscribe(mqttPrefix + "/lights/alert");
+  mqtt.subscribe(mqttPrefix + "/lights/christmas/color");
+  mqtt.subscribe(mqttPrefix + "/lights/christmas/striped");
 
   Serial.println("MQTT connected!");
 }
 
+void alert() {
+    static uint8_t colorIndex = 0;
+    static int blinkTimes[12] = { 100, 100, 100, 100, 100, 1000, 100, 100, 100, 100, 1000 };
+    int blinked = 0;
+    while (blinked < 12) {
+      for( int i = 0; i < NUM_LEDS; i++) {
+          if (blinked % 2) {
+            leds[i] = CRGB::Black;
+          } else {
+            leds[i] = CRGB::Blue;
+          }
+        }
+      colorIndex += 3;
+      FastLED.show();
+      delay(blinkTimes[blinked]);
+      colorIndex = 0;
+      blinked++;
+    }
+  }
+
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
-  if (topic == "/lights/all") {
+  if (topic == (mqttPrefix + "/lights/all")) {
     if (payload == "on") {
       lightsOn = true;
       Serial.println("lights on!");
@@ -119,15 +155,35 @@ void messageReceived(String &topic, String &payload) {
       Serial.println("lights out!");
     }
   }
-  if (topic == "/lights/christmas") {
+  if (topic == (mqttPrefix + "/lights/christmas")) {
     if (payload == "on") {
       lightsOn = true;
+      christmas = true;
       Serial.println("lights on!");
     }
     if (payload == "off") {
       lightsOn = false;
       Serial.println("lights out!");
     }
+  }
+  if (topic == (mqttPrefix + "/lights/christmas/color")) {
+      lightsOn = true;
+      christmas = false;
+      color = atoi(payload.c_str());
+      Serial.println("lights on and colored!");
+  }
+  if (topic == (mqttPrefix + "/lights/christmas/striped")) {
+    if (payload == "1") {
+      striped = true;
+      Serial.println("color striped!");
+    }
+    if (payload == "0") {
+      striped = false;
+      Serial.println("color continuous!");
+    }
+  }
+  if (topic == (mqttPrefix + "/lights/alert")) {
+    alert();
   }
 }
 
@@ -149,16 +205,19 @@ void addGlitter( fract8 chanceOfGlitter)
   }
 }
 
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        SetupChristmasPalette();
-        currentBlending = LINEARBLEND;
+void HandlePallete()
+{   
+  if (christmas) {
+    SetupChristmasPalette();
+  } else {
+    if (striped) {
+      SetupStripedColorPalette();
+    } else {
+      SetupColorPalette();
     }
+  }
+  //There is a chance to controll this later
+  currentBlending = LINEARBLEND;
 }
 void SetupChristmasPalette()
 {
@@ -171,3 +230,20 @@ void SetupChristmasPalette()
     currentPalette[12] = CRGB::Green;
     
 }
+void SetupColorPalette()
+{
+    //all 16 palette entries to color
+    fill_solid( currentPalette, 16, CHSV(color,255,255));
+    
+}
+void SetupStripedColorPalette()
+{
+    //all 16 palette entries to black...
+    fill_solid( currentPalette, 16, CRGB::Black);
+    //and every forth to color
+    currentPalette[0] = CHSV(color,255,255);
+    currentPalette[4] = CHSV(color,255,255);
+    currentPalette[8] = CHSV(color,255,255);
+    currentPalette[12] = CHSV(color,255,255);
+}
+
